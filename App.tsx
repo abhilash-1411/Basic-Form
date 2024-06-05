7567e1e317b8cb59a1bad4667c1d675637c7f726
abhilash-1411 <Abhilash.Chandravanshi@adixoo.com>
2024-06-05 16:49:04 +0530

    Input form

--- App.tsx
+++ App.tsx
@@ -1,118 +1,85 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+
+import React, { useState } from 'react';
+
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
+  
   StyleSheet,
   Text,
-  useColorScheme,
+  Button,
+
+  TextInput,
+
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App=()=> {
+  const [name,setName]=useState()
+  const [email,setEmail]=useState()
+  const [password,setPassword]=useState()
+  const [display,setDsiplay]=useState(false)
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  const resetFormData=()=>{ 
+    setDsiplay(false);
+    setEmail(" ");
+    setName(" ");
+    setPassword(" ");
+  }
+ 
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View>
+      <Text>Basic Form</Text>
+      <TextInput 
+      style={styles.textInput}
+      placeholder='Enter User Name'
+      onChangeText={(text) =>setName(text)}
+      value={name}
+       />
+        <TextInput 
+      style={styles.textInput}
+      placeholder='Enter User Password'
+      secureTextEntry={true}
+      onChangeText={(text) =>setPassword(text)}
+      value={password}
+       />
+        <TextInput 
+      style={styles.textInput}
+      placeholder='Enter User Email'
+      onChangeText={(text) =>setEmail(text)}
+      value={email}
+       />
+       <View style={{marginBottom:10}}>
+
+       <Button color='green' title='Print Details' onPress={()=>setDsiplay(true)}/>
+       </View>
+       <Button title='Clear Details' onPress={resetFormData}/>
+       <View>
+        {
+          display?
+          <View>
+            <Text style={{fontSize:20}}>User Name is :{name}</Text>
+            <Text style={{fontSize:20}}>User Password is :{password}</Text>
+            <Text style={{fontSize:20}}>User Email is :{email}</Text>
+            </View>:null
+        }
+       </View>
     </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    
+       
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+ textInput:{
+  fontSize:18,
+  color:'red',
+  borderWidth:2,
+  borderColor:'blue',
+  margin :10
+
+ }
+  
 });
 
 export default App;
